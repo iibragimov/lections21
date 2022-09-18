7d4a899eb0d51b9470012ce4a922144974335aee
iibragimov <ilmir.soap@gmail.com>
2022-09-19 00:52:49 +0300

    hw1

--- lection01/homework/main.go
+++ lection01/homework/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+type Color string
+
+const (
+	Reset  Color = "\033[0m"
+	Red    Color = "\033[31m"
+	Green  Color = "\033[32m"
+	Yellow Color = "\033[33m"
+	Blue   Color = "\033[34m"
+	Purple Color = "\033[35m"
+	Cyan   Color = "\033[36m"
+	Gray   Color = "\033[37m"
+	White  Color = "\033[97m"
+)
 
 func main() {
-	fmt.Println("\033[31mHello \033[0mWorld") // https://www.shellhacks.com/bash-colors/
+	sandglass(11, '@', Green)
+}
+
+func sandglass(size int, char rune, color Color) {
+	sand := make([][]rune, size)
+	sand[0] = makeRow(size, char)
+	sand[size-1] = makeRow(size, char)
+	half := (size + 1) / 2
+	for i := 1; i < half; i++ {
+		u := makeRow(size, ' ')
+		d := makeRow(size, ' ')
+		u[i] = char
+		u[size-1-i] = char
+		d[i] = char
+		d[size-1-i] = char
+		sand[i] = u
+		sand[size-1-i] = d
+	}
+
+	for _, v := range sand {
+		println(string(color) + string(v))
+	}
+	print(Reset)
+}
+
+func makeRow(size int, char rune) []rune {
+	row := make([]rune, size)
+	for i := range row {
+		row[i] = char
+	}
+	return row
 }
